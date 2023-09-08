eb94f14867e1f26039a81c1eba3b2c271d1c1fab
Alejo González <alejog240@gmail.com>
2023-09-08 16:05:44 -0300

    Register changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
-import Register from "./components/Register";
+import ModalRegister from "./components/ModalRegister";
 import ModalLogin from "./components/ModalLogin";
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/registro" element={<Register />} />
+        <Route path="/registro" element={<ModalRegister />} />
         <Route path="/iniciar-sesion" element={<ModalLogin />} />
       </Routes>
     </>
