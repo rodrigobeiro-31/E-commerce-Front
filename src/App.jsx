a0e1fd244aca1cece608e7fb46ec8bb34a4f8c07
Kathia01 <kathia.olaverry@gmail.com>
2023-09-09 15:00:54 -0300

    Products view

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
-import Register from "./components/Register";
-import Login from "./components/Login";
 import Products from "./components/Products";
 import Product from "./components/Product";
 import About from "./components/About";
@@ -17,8 +15,6 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/registro" element={<Register />} />
-        <Route path="/iniciar-sesion" element={<Login />} />
         <Route path="/products" element={<Products />} />
         <Route path="/product" element={<Product />} />
         <Route path="/about-us" element={<About />} />
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,22 +1,138 @@
-import React from 'react'
+import React from "react";
+import Accordion from 'react-bootstrap/Accordion';
+import { BsFill0CircleFill } from "react-icons/bs";
+
 
 function Product() {
   return (
-    <div class="card mb-3" style="max-width: 540px;">
-  <div class="row g-0">
-    <div class="col-md-4">
-      <img src="https://unareceta.com/wp-content/uploads/2017/06/receta-de-muffins-con-pepitas-de-chocolate.jpg" class="img-fluid rounded-start" alt="..."/>
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">Chocolate chip muffins</h5>
-        <p class="card-text">Los muffins de chispas de chocolate son una auténtica delicia para los amantes del chocolate. Nuestros muffins, recién horneados y llenos de chispas de chocolate derretidas, son la elección perfecta para satisfacer tus antojos dulces en cualquier momento del día. Cada mordisco es una explosión de sabor, con una masa esponjosa y suave que se combina perfectamente con las irresistibles chispas de chocolate. Ya sea para el desayuno, un tentempié o un capricho a media tarde, nuestros muffins de chispas de chocolate son la elección ideal. ¡Ven y descubre el placer del chocolate en cada bocado!"</p>
-        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+    <>
+    <div className="container d-flex justify-content-center align-items-center mt-5">
+    <div className="card mb-3">
+      <div className="row g-0">
+        <div className="col-6">
+          <img
+            src="https://unareceta.com/wp-content/uploads/2017/06/receta-de-muffins-con-pepitas-de-chocolate.jpg"
+           
+            style={{ maxHeight: '100%', width: '100%', objectFit: 'cover' }}
+          />
+        </div>
+        <div className="col-6">
+          <div className="card-body">
+            <h5 className="text-end">Panadería</h5>
+            <h3>Chocolate chip muffins</h3>
+            <p className="card-text">
+              Nuestros muffins, recién horneados y llenos de chispas de
+              chocolate derretidas, son la elección perfecta para satisfacer tus
+              antojos dulces en cualquier momento del día. Cada mordisco es una
+              explosión de sabor, con una masa esponjosa y suave que se combina
+              perfectamente con las irresistibles chispas de chocolate. Ya sea
+              para el desayuno, o un capricho a media tarde, nuestros muffins de
+              chispas de chocolate son la elección ideal. ¡Ven y descubre el
+              placer del chocolate en cada bocado!"
+            </p>
+            <p className="text-body-secondary">STOCK: 143</p>
+
+            <div className="d-flex justify-content-between">
+              <p className="card-text">$USD: 2.60</p>
+              <a href="#" className="btn btn-secondary d-inline">
+                Ir al carrito
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</div>
-  )
+    </div>
+
+    
+
+
+  <div className="container mt-5">
+    <Accordion defaultActiveKey="0">
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>Infomación sobre pedidos y envios</Accordion.Header>
+        <Accordion.Body>
+        En nuestra tienda, la satisfacción del cliente es nuestra prioridad. Realizar un pedido es fácil y conveniente. Simplemente selecciona tus productos favoritos, proporciona tus detalles de envío y elige tu método de pago preferido. Nuestro equipo de expertos panaderos preparará con cariño y cuidado tus productos frescos antes de enviarlos de forma segura y puntual a tu puerta. Confía en nosotros para ofrecerte una experiencia de compra excepcional, donde la calidad y la frescura son lo primero.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>Métodos de pago</Accordion.Header>
+        <Accordion.Body>
+        Queremos que tu experiencia de compra sea cómoda y segura. Ofrecemos una variedad de métodos de pago para adaptarnos a tus necesidades. Puedes pagar con tarjeta de crédito o débito. También aceptamos transferencias bancarias y, si lo prefieres, puedes pagar en efectivo en nuestra tienda física al momento de la entrega. Tu seguridad y conveniencia son fundamentales para nosotros, por lo que puedes elegir el método que mejor te convenga.
+        
+        
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="2">
+        <Accordion.Header>Aviso legal</Accordion.Header>
+        <Accordion.Body>
+        Este sitio web está diseñado para proporcionar información sobre nuestros productos y permitirte realizar compras en línea. Nos esforzamos por mantener la precisión y actualidad de la información, pero no garantizamos la disponibilidad constante de todos los productos. Además, cualquier información proporcionada en este sitio no debe considerarse asesoramiento legal, médico o profesional. Al hacer una compra, aceptas nuestros términos y condiciones, incluyendo nuestra política de privacidad. Si tienes alguna pregunta o inquietud, no dudes en contactarnos. Tu satisfacción es nuestra prioridad.
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+    </div>
+  
+
+
+
+
+    <div className="container mt-5">
+        <h2 className="text-center mb-3">Productos que te pueden interesar:</h2>
+        <div className="row">
+        <div className="col-3 mb-4">
+            <div class="card">
+              <img
+                src="https://unareceta.com/wp-content/uploads/2017/06/receta-de-muffins-con-pepitas-de-chocolate.jpg"
+                className="card-img-top"
+                alt="muffins"
+              />
+              <div className="card-body">
+                <a href="#" className="text-decoration-none text-dark"><h5 className="text-center">Muffins de chocolate</h5></a>
+              </div>
+            </div>
+          </div>
+          <div className="col-3 mb-4">
+            <div class="card">
+              <img
+                src="https://unareceta.com/wp-content/uploads/2017/06/receta-de-muffins-con-pepitas-de-chocolate.jpg"
+                className="card-img-top"
+                alt="muffins"
+              />
+              <div className="card-body">
+                <a href="#" className="text-decoration-none text-dark"><h5 className="text-center">Muffins de chocolate</h5></a>
+              </div>
+            </div>
+          </div>
+          <div className="col-3 mb-4">
+            <div class="card">
+              <img
+                src="https://unareceta.com/wp-content/uploads/2017/06/receta-de-muffins-con-pepitas-de-chocolate.jpg"
+                className="card-img-top"
+                alt="muffins"
+              />
+              <div className="card-body">
+                <a href="#" className="text-decoration-none text-dark"><h5 className="text-center">Muffins de chocolate</h5></a>
+              </div>
+            </div>
+          </div>
+          <div className="col-3 mb-4">
+            <div class="card">
+              <img
+                src="https://unareceta.com/wp-content/uploads/2017/06/receta-de-muffins-con-pepitas-de-chocolate.jpg"
+                className="card-img-top"
+                alt="muffins"
+              />
+              <div className="card-body">
+                <a href="#" className="text-decoration-none text-dark"><h5 className="text-center">Muffins de chocolate</h5></a>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+    
+
+    </>
+  );
 }
 
-export default Product
+export default Product;
