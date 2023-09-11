3d7a68fed6b8267bf510671b253743bf5ea55453
Alejo González <alejog240@gmail.com>
2023-09-11 14:58:55 -0300

    Updated Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,120 +1,79 @@
 // import { useEffect } from "react";
 // import { useSelector } from "react-redux";
 // import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
+import Carousel from 'react-bootstrap/Carousel';
+import Image from 'react-bootstrap/Image';
 
 function Home() {
+  const [products, setProducts] = useState();
   // const navigate = useNavigate();
   // const user = useSelector((state) => state.user);
 
   // useEffect(() => {
   //   if (!user) return navigate("/iniciar-sesion");
   // }, []);
-
-  return (
-    <div className="container">
-      {/* El carrousel tiene que ser con React Bootstrap */}
-      <div className="row">
-        <div id="carouselExampleIndicators" className="carousel slide">
-          <div className="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-          </div>
-          <div className="carousel-inner">
-            <div className="carousel-item active">
-              <img src="/imgs/banner-1.jpg" className="d-block w-100" alt="..." />
+  useEffect(() => {
+    const getProducts = async () => {
+      const response = await axios({
+        method: "GET",
+        url: "http://localhost:3000/products/top",
+        /* headers: {
+          Authorization: "Bearer " + (user && user.token),
+        }, */
+      });
+      response && setProducts(response.data);
+    };
+    getProducts();
+  }, []);
+  
+  return products &&
+    <>
+      <div className="container">
+        <div className="row">
+          <Carousel >
+            <Carousel.Item>
+              <Image src="./imgs/banner-1.jpg" fluid />
+            </Carousel.Item>
+            <Carousel.Item>
+              <Image src="./imgs/banner-2.jpg" fluid />
+            </Carousel.Item>
+            <Carousel.Item>
+              <Image src="./imgs/banner-3.jpg" fluid />
+            </Carousel.Item>
+          </Carousel>
+        </div>
+        <div className="row d-flex text-center">
+          <h1 className="mt-2">Home</h1>
+          <h5 className="mt-1">Scroll down to see more</h5>
+        </div>
+        <div className="row mt-5">
+          <h3 className="mb-5">Top products</h3>
+          {products.map((product, id) => (
+            <div key={id} className="col-3 mb-4">
+              <div className="card">
+                <img
+                  src={product.image}
+                  className="card-img-top"
+                  alt={product.name}
+                />
+                <div className="card-body">
+                  <NavLink to={`/products/${product.slug}`}>
+                    <h5 className="card-title">{product.name}</h5>
+                  </NavLink>
+                  <p className="card-text">$USD: {product.price}</p>
+                  <a href="#" className="btn btn-secondary float-end">
+                    Agregar al carrito
+                  </a>
+                </div>
+              </div>
             </div>
-            <div className="carousel-item">
-              <img src="/imgs/banner-2.jpg" className="d-block w-100" alt="..." />
-            </div>
-            <div className="carousel-item">
-              <img src="/imgs/banner-3.jpg" className="d-block w-100" alt="..." />
-            </div>
-          </div>
-          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Previous</span>
-          </button>
-          <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Next</span>
-          </button>
+          ))}
         </div>
       </div>
-      <div className="row d-flex text-center">
-        <h1 className="mt-2">Home</h1>
-        <h5 className="mt-1">Scroll down to see more</h5>
-      </div>
-      <div className="row mt-5">
-        <h3 className="mb-2">New products</h3>
-        <div className="col-3">
-          <div className="card mb-3">
-            <div className="row g-0">
-              <div className="col-md-4 align-self-center">
-                <img src="/imgs/products/baguette.png" className="img-fluid rounded-start" alt="..." />
-              </div>
-              <div className="col-md-8">
-                <div className="card-body">
-                  <h5 className="card-title">Baguette</h5>
-                  <p className="card-text">$2.00</p>
-                  <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="col-3">
-          <div className="card mb-3">
-            <div className="row g-0">
-              <div className="col-md-4 align-self-center">
-                <img src="/imgs/products/danish.png" className="img-fluid rounded-start" alt="..." />
-              </div>
-              <div className="col-md-8">
-                <div className="card-body">
-                  <h5 className="card-title">Danish</h5>
-                  <p className="card-text">$1.00</p>
-                  <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="col-3">
-          <div className="card mb-3">
-            <div className="row g-0">
-              <div className="col-md-4 align-self-center">
-                <img src="/imgs/products/croissant.png" className="img-fluid rounded-start" alt="..." />
-              </div>
-              <div className="col-md-8">
-                <div className="card-body">
-                  <h5 className="card-title">Croissant</h5>
-                  <p className="card-text">$1.50</p>
-                  <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="col-3">
-          <div className="card mb-3">
-            <div className="row g-0">
-              <div className="col-md-4 align-self-center">
-                <img src="/imgs/products/espresso.jpg" className="img-fluid rounded-start" alt="..." />
-              </div>
-              <div className="col-md-8">
-                <div className="card-body">
-                  <h5 className="card-title">Espresso</h5>
-                  <p className="card-text">$3.00</p>
-                  <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-    </div >
-  );
+    </>
 }
 
 export default Home;
