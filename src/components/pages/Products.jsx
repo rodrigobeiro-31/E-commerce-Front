e8885c6786b013ab5398db16562ee68813ad2041
Kathia01 <kathia.olaverry@gmail.com>
2023-09-12 09:19:50 -0300

    products

--- src/components/pages/Products.jsx
+++ src/components/pages/Products.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../components/products.css"
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { NavLink, Link } from "react-router-dom";
@@ -23,7 +24,7 @@ function Products() {
     products && (
       <>
         <div className="container mt-5">
-          <h2 className="text-center">Todos los productos</h2>
+          <h2 className="text-center">All products</h2>
           <div className="row">
             <div className="col d-flex justify-content-end">
               <h4 className="m-0">Filter by:</h4>
@@ -58,7 +59,7 @@ function Products() {
           </div>
         </div>
 
-        <div className="container mt-5">
+        {/* <div className="container mt-5">
           <div className="row">
             {products.map((product, id) => (
               <div key={id} className="col-3 mb-4">
@@ -81,7 +82,65 @@ function Products() {
               </div>
             ))}
           </div>
+        </div> */}
+
+{/* <div className="container mt-5">
+          <div className="row">
+            {products.map((product, id) => (
+              <div key={id} className="col-3 mb-4">
+                <div className="card">
+                  <img
+                    src={product.image}
+                    className="card-img-top"
+                    alt={product.name}
+                  />
+                  <div class="card-img-overlay">
+                    <h6 className="text-center">Add to cart</h6>
+                  </div>
+                  <div className="card-body">
+                    <NavLink to={`/products/${product.slug}`}>
+                      <h5 className="card-title">{product.name}</h5>
+                    </NavLink>
+                    <p className="card-text">$USD: {product.price}</p>
+                    <a href="#" className="btn btn-secondary float-end">
+                      Ir al carrito
+                    </a>
+                  </div>
+                 
+                </div>
+              </div>
+            ))}
+          </div>
+        </div> */}
+
+<div className="container mt-5">
+  <div className="row">
+    {products.map((product, id) => (
+      <div key={id} className="col-3 mb-4">
+        <div className="productsCard">
+          <div className="position-relative">
+            <img
+              src={product.image}
+              className="card-img-top"
+              alt={product.name}
+            />
+            <div className="card-img-overlay d-flex flex-column justify-content-end p-0 addToCart">
+              <h6 className="text-center mt-auto">ADD TO CART</h6>
+            </div>
+          </div>
+          <div className="card-body">
+            <NavLink  className="text-decoration-none text-dark" to={`/products/${product.slug}`}>
+              <h5 className="card-title">{product.name}</h5>
+            </NavLink>
+            <p className="card-text">$USD: {product.price}</p>
+           
+          </div>
         </div>
+      </div>
+    ))}
+  </div>
+</div>
+
       </>
     )
   );
