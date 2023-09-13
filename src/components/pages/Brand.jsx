3d0e0beac59fc246b8e5e0275f8bc6dc152c4db1
Kathia01 <kathia.olaverry@gmail.com>
2023-09-13 11:54:14 -0300

    ScrollTo

---
+++ src/components/pages/Brand.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Brand() {
+  return (
+   <div className="container">
+    <div className="row">
+        <div className="col"></div>
+        <div className="col"></div>
+        <div className="col"></div>
+        <div className="col"></div>
+    </div>
+   </div>
+  )
+}
+
+export default Brand
