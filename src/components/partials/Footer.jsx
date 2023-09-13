377570f925504770e0aeccd37fef43ed868b1faa
rodrigobeiro-31 <rodrigo.beiro@student.ha.dev>
2023-09-13 15:30:15 -0300

    Footer

--- src/components/partials/Footer.jsx
+++ src/components/partials/Footer.jsx
@@ -4,22 +4,60 @@ import "./Footer.css";
 function Footer() {
   return (
     <footer className="d-flex justify-content-between align-items-center  text-white">
-      <div className="">HackCommerce</div>
-      <div className="nav">
-        <NavLink to="#" className="nav-link py-1 footer-links">
-          START
+      <div className="">
+        <img
+          src="imgs/doppios-white-logo.png"
+          alt="Doppios Logo"
+          className="nav-logo"
+        />
+      </div>
+      <div className="nav text-white">
+        <NavLink to="/" className="footerLink nav-link">
+          HOME
         </NavLink>
-        <NavLink to="#" className="nav-link py-1 footer-links">
-          ABOUT OUR PROJECT
-        </NavLink>
-        <NavLink to="# " className="nav-link py-1 footer-links">
+        <NavLink to="/products" className="footerLink nav-link">
           PRODUCTS
         </NavLink>
-        <NavLink to="#" className="nav-link py-1 footer-links">
+        <NavLink to="/about-us " className="footerLink nav-link">
+          ABOUT THIS PROJECT
+        </NavLink>
+        <NavLink to="/contact" className="footerLink nav-link">
           CONTACT
         </NavLink>
       </div>
-      <div>SOCIAL:</div>
+      <div className="d-flex justify-content-between gap-2">
+        <h6 className="footerLink nav-link text-white align-self-center">
+          SOCIAL :
+        </h6>
+        <a
+          href="https://www.facebook.com/"
+          target="blank"
+          className=" footerLink text-white mb-2 a-icons"
+        >
+          <i className="fa-brands fa-facebook"></i>
+        </a>
+        <a
+          href="https://www.instagram.com/"
+          target="blank"
+          className=" footerLink text-white a-icons"
+        >
+          <i className="fa-brands fa-instagram"></i>
+        </a>
+        <a
+          href="https://twitter.com/"
+          target="blank"
+          className=" footerLink text-white a-icons"
+        >
+          <i className="fa-brands fa-twitter"></i>
+        </a>
+        <a
+          href="https://www.linkedin.com/"
+          target="blank"
+          className=" footerLink text-white a-icons"
+        >
+          <i className="fa-brands fa-linkedin"></i>
+        </a>
+      </div>
     </footer>
   );
 }
