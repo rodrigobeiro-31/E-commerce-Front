752774c4bff57d1679eee5b985faa104f16176d1
Alejo González <alejog240@gmail.com>
2023-09-13 15:59:23 -0300

    Changed Contact Style

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useState } from "react";
 import "./Contact.css";
 import axios from "axios";
+import { BsFillTelephoneFill } from "react-icons/bs";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
@@ -55,131 +56,131 @@ function Contact() {
   };
 
   return (
-    <div className="column arriba  ">
-      <div className=" d-flex-column  align-self-center align-content-between blur ">
-        <div className="row">
-          <div className="col-6">
-            <div className="container">
-              <div>
-                <h2 className="mt-5 pt-4">Send us a message.</h2>
-                <h3>We will be responding to you shortly.</h3>
+    <>
+      <div className="container-fluid d-flex align-items-center flex-column justify-content-center m-0 p-0 contact-banner">
+      </div>
+      <div className="container mb-5 mt-5 contact-container custom-background">
+        <div className=" d-flex-column  align-self-center align-content-between">
+          <div className="row">
+            <div className="col-6">
+              <div className="container">
+                <div>
+                  <h2 className="fw-bold pt-4">Contact us</h2>
+                  <p className="fw-normal">We will be responding to you shortly.</p>
+                </div>
+                <form onSubmit={sendForm} className="row g-3">
+                  <div className="col-5">
+                    <label htmlFor="firstName" className="form-label">
+                      Firstname :
+                    </label>
+                    <input
+                      className="form-control"
+                      name="firstname"
+                      id="firstname"
+                      placeholder="Firstname..."
+                      value={formData.firstname}
+                      onChange={handleChange}
+                    />
+                  </div>
+                  <div className="col-5">
+                    <label htmlFor="lastname" className="form-label">
+                      Lastname :
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="lastname"
+                      id="lastname"
+                      placeholder="Lastname..."
+                      value={formData.lastname}
+                      onChange={handleChange}
+                    />
+                  </div>
+                  <div className="col-5">
+                    <label htmlFor="ilastName" className="form-label">
+                      Phone number :
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="fone"
+                      id="fone"
+                      placeholder="Phone number..."
+                      value={formData.fone}
+                      onChange={handleChange}
+                    />
+                  </div>
+                  <div className="col-md-5">
+                    <label htmlFor="direction" className="form-label">
+                      Address :
+                    </label>
+                    <input
+                      type="name"
+                      className="form-control"
+                      name="direction"
+                      id="direction"
+                      placeholder="Address..."
+                      value={formData.direction}
+                      onChange={handleChange}
+                    />
+                  </div>
+                  <div className="col-10">
+                    <label htmlFor="fone" className="form-label">
+                      Affair :
+                    </label>
+                    <textarea
+                      type="name"
+                      className="form-control"
+                      name="affaire"
+                      id="affaire"
+                      placeholder="Affair..."
+                      value={formData.affaire}
+                      onChange={handleChange}
+                    />
+                  </div>
+
+                  <div className="col-md-5">
+                    <label htmlFor="options" className="form-label">
+                      Options :
+                    </label>
+                    <select
+                      className="form-select"
+                      type="text"
+                      name="options"
+                      id="options"
+                      placeholder="Options..."
+                      value={formData.options}
+                      onChange={handleChange}
+                    >
+                      <option>Family birthday</option>
+                      <option>Coffee shop</option>
+                      <option>Weddings</option>
+                      <option>whim</option>
+                    </select>
+                  </div>
+                  <div className="col-5 text-center pt-1">
+                    <button
+                      type="submit"
+                      className="btn  m-4 text-center btn-modal "
+                    >
+                      Send{" "}
+                    </button>
+                  </div>
+                </form>
               </div>
-              <form onSubmit={sendForm} className="row g-3">
-                <div className="col-5">
-                  <label htmlFor="firstName" className="form-label">
-                    Firstname :
-                  </label>
-                  <input
-                    className="form-control"
-                    name="firstname"
-                    id="firstname"
-                    placeholder="Firstname..."
-                    value={formData.firstname}
-                    onChange={handleChange}
-                  />
-                </div>
-                <div className="col-5">
-                  <label htmlFor="lastname" className="form-label">
-                    Lastname :
-                  </label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    name="lastname"
-                    id="lastname"
-                    placeholder="Lastname..."
-                    value={formData.lastname}
-                    onChange={handleChange}
-                  />
-                </div>
-                <div className="col-5">
-                  <label htmlFor="ilastName" className="form-label">
-                    Phone number :
-                  </label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    name="fone"
-                    id="fone"
-                    placeholder="Phone number..."
-                    value={formData.fone}
-                    onChange={handleChange}
-                  />
-                </div>
-                <div className="col-md-5">
-                  <label htmlFor="direction" className="form-label">
-                    Address :
-                  </label>
-                  <input
-                    type="name"
-                    className="form-control"
-                    name="direction"
-                    id="direction"
-                    placeholder="Address..."
-                    value={formData.direction}
-                    onChange={handleChange}
-                  />
-                </div>
-                <div className="col-10">
-                  <label htmlFor="fone" className="form-label">
-                    Affair :
-                  </label>
-                  <textarea
-                    type="name"
-                    className="form-control"
-                    name="affaire"
-                    id="affaire"
-                    placeholder="Affair..."
-                    value={formData.affaire}
-                    onChange={handleChange}
-                  />
-                </div>
-
-                <div className="col-md-5">
-                  <label htmlFor="options" className="form-label">
-                    Options :
-                  </label>
-                  <select
-                    className="form-select"
-                    type="text"
-                    name="options"
-                    id="options"
-                    placeholder="Options..."
-                    value={formData.options}
-                    onChange={handleChange}
-                  >
-                    <option>Family birthday</option>
-                    <option>Coffee shop</option>
-                    <option>Weddings</option>
-                    <option>whim</option>
-                  </select>
-                </div>
-                <div className="col-5 text-center mt-5 pt-4 ">
-                  <button
-                    type="submit"
-                    className="btn  m-4 text-center btn-modal "
-                  >
-                    Send{" "}
-                  </button>
-                </div>
-              </form>
             </div>
-          </div>
-          <div className="col  d-flex-column  align-self-center align-content-between blur ">
-            <h1 className="m-5"> Contact information...</h1>
-            <h4 className="m-3"> E-commerce ONLINE: every day ...</h4>
-            <h4 className="m-3">
-              26061980 - 093 838890 - Monday to Friday de 9:00 a 19:00 hs{" "}
-            </h4>
-            <h4 className="m-3">
-              093 838890 - Saturdays and Sundays de 12:00 a 19:00 hs
-            </h4>
-
-            <h3 className="m-5">Ecommerce@doppios.com</h3>
+            <div className="col-6 d-flex-column align-self-center align-content-between">
+              <h4 className="fw-bold mb-5">PHONE NUMBER</h4>
+              <p>Mondays through Fridays from 9:00 am to 19:00 pm</p>
+              <h5 className="ps-5 mt-3"><BsFillTelephoneFill className="me-2"/> 2606 1980</h5>
+              <h5 className="ps-5 mb-5"><BsFillTelephoneFill className="me-2"/> 093 838 890</h5>
+              <p>Saturdays and Sundays from 12:00 am to 19:00 pm</p>
+              <h5 className="ps-5 mt-3"><BsFillTelephoneFill className="me-2"/> 093 838 890</h5>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -26,17 +26,7 @@ function Home() {
   return products &&
     <>
       <div className="container-fluid p-0 g-0">
-        {/* <Carousel>}
-          {/* <Carousel.Item>
-            <Image src="./imgs/home-carousel-1.jpg" className="w-100" fluid />
-          </Carousel.Item>
-          <Carousel.Item>
-            <Image src="./imgs/home-carousel-2.jpg" className="w-100" fluid />
-          </Carousel.Item>
-          <Carousel.Item>
-            <Image src="./imgs/home-carousel-3.jpg" className="w-100" fluid />
-          </Carousel.Item> */}
-        {/* </Carousel> */}
+
         <img id="homeBanner1" src="./imgs/home-banner-1.jpg" alt="Coffee beans" />
         <div className="home-gd-overlay"></div>
         <div id="homeTitle" className="row d-flex justify-content-center m-0 p-0 mb-5">
@@ -51,7 +41,7 @@ function Home() {
               <div className="card-body d-flex flex-column align-items-center p-5">
                 <h3 className="card-title mb-5  fw-bold">About Doppio's</h3>
                 <p className="card-text mb-4">At Doppio's Coffee & Bakery, we blend the rich tradition of artisanal coffee crafting with the comforting aroma of freshly baked bread to create an inviting haven for coffee enthusiasts and pastry lovers alike. Our name, "Doppio," embodies our commitment to excellence, as it refers to the Italian term for "double." Just as a doppio espresso is a double shot of pure espresso, we aim to double your satisfaction with every visit.</p>
-                <p className="card-text">With a passion for quality and a dedication to delighting your senses, our coffee is sourced from the finest beans and expertly roasted to perfection, ensuring each sip is a symphony of flavor. Complementing our coffee, our bakery boasts an array of handcrafted bread, pastries, and confections, baked fresh daily. Whether you're seeking the bold, invigorating notes of a doppio espresso or the comforting embrace of a warm croissant, Doppio's is where genuine Italian tradition meets warm-hearted hospitality. Join us in savoring the simple pleasures of life, one cup and one bite at a time. Welcome to Doppio's, where every visit is a double delight.</p>
+                <p className="card-text">With a passion for quality and a dedication to delighting your senses, our coffee is sourced from the finest beans and expertly roasted to perfection, ensuring each sip is a symphony of flavor. Complementing our coffee, our bakery boasts an array of handcrafted bread, pastries, and confections, baked fresh daily. Join us in savoring the simple pleasures of life, one cup and one bite at a time. Welcome to Doppio's, where every visit is a double delight.</p>
                 <img className="img align-self-center mt-4 mb-0 opacity-75 home-section-logo" src="/imgs/doppios-black-logo.png" alt="Doppio's logo" />
               </div>
             </div>
@@ -98,7 +88,7 @@ function Home() {
               <img
                 src="/imgs/coffee-bean-icon.png"
                 alt="label"
-                className="img-fluid mt-3 opacity-75 custom-image" />
+                className="img-fluid mt-3 custom-image" />
               <h5 className="text-center mt-3 fw-semibold">Sustainable</h5>
               <p className="text-center fw-light">
                 Committed to a sustainable future, we take pride in crafting our
@@ -108,7 +98,7 @@ function Home() {
             </div>
           </div>
         </div>
-        <div className="row mt-5 m-0 p-0">
+        <div className="row mt-5 mb-5 m-0 p-0">
           <h3 className="mb-4 fw-bold text-center bg-light p-2">THIS WEEK'S PICKS</h3>
           {products.map((product, id) => (
             <div key={id} className="col-3 mainCard mb-5">
@@ -134,9 +124,9 @@ function Home() {
                     className="text-decoration-none productName"
                     to={`/products/${product.slug}`}
                   >
-                    <h5 className="card-title text-uppercase fw-bold">{product.name}</h5>
+                    <h5 className="card-title">{product.name}</h5>
                   </NavLink>
-                  <p className="card-text priceText mt-2  fst-italic">
+                  <p className="card-text priceText mt-2">
                     $USD: {product.price}
                   </p>
                 </div>
@@ -144,6 +134,17 @@ function Home() {
             </div>
           ))}
         </div>
+        <Carousel>
+          <Carousel.Item>
+            <Image src="./imgs/home-carousel-1.jpg" className="w-100" fluid />
+          </Carousel.Item>
+          <Carousel.Item>
+            <Image src="./imgs/home-carousel-2.jpg" className="w-100" fluid />
+          </Carousel.Item>
+          <Carousel.Item>
+            <Image src="./imgs/home-carousel-3.jpg" className="w-100" fluid />
+          </Carousel.Item>
+        </Carousel>
       </div>
     </>
 }
