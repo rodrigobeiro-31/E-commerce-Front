0fbbb9e477885800ecda65b24016a4d2934aa0d2
Alejo González <alejog240@gmail.com>
2023-09-21 14:57:55 -0300

    Remove/Change quantity en Checkout

--- src/components/pages/Products.jsx
+++ src/components/pages/Products.jsx
@@ -28,7 +28,7 @@ function Products() {
           Authorization: "Bearer " + (user && user.token),
         }, */
       });
-      response && setProducts(response.data.products);
+      response && setProducts(response.data.filteredProducts);
     };
     getProducts();
   }, []);
@@ -41,72 +41,32 @@ function Products() {
   return (
     products && (
       <>
-        {console.log(products)}
         <div className="container-fluid main-container p-0">
           <div className="container-fluid d-flex align-items-center flex-column justify-content-center imageContainer m-0 p-0">
             <h2 className="text-white d-flex justify-content-center align-items-center title">
               PRODUCTS
             </h2>
           </div>
-
-          {/* <div className="container mt-5">
-            <div className="row d-flex flex-wrap g-3 m-0">
-              {products.map((product, id) => (
-                <div key={id} className="col-3 mainCard mb-5">
-                  {console.log(product.slug)}
-
-                  <div className="productsCard">
-                    <div className="position-relative">
-                      <img
-                        src={`https://mcbzesritumxqjtbullp.supabase.co/storage/v1/object/public/products/${product.image}?t=2023-09-19T13%3A20%3A01.474Z`}
-                        className="card-img imgCard"
-                        alt={product.name}
-                      />
-                      <div
-                        className="card-img-overlay d-flex flex-column justify-content-end p-0 addToCart"
-                        onClick={() => handleAddCart(product)}
-                      >
-                        <h6 className="text-center mt-auto p-1">
-                          {" "}
-                          <BsFillBagFill /> ADD TO CART
-                        </h6>
-                      </div>
-                    </div>
-                    <div
-                      className="card-body p-1 ps-4 mt-3"
-                      style={{ backgroundColor: "black" }}
-                    >
-                      <NavLink
-                        className="text-decoration-none productName"
-                        to={`/products/${product.slug}`}
-                      >
-                        <p className="card-title text-uppercase fw-bold">
-                          {product.name}
-                        </p>
-                      </NavLink>
-                      <p className="card-text priceText mt-2 fst-italic">
-                        Price: $ {product.price}
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              ))}
-            </div>
-          </div> */}
-
           <div className="container mt-5">
+            <div className="row">
+            <select className="col-3 form-select mb-4" aria-label="Default select example">
+            <option selected>Filter by category</option>
+            <option value="1">Cafe</option>
+            <option value="2">Bakery</option>
+            <option value="3">Cakes</option>
+            <option value="4">Coffee beans</option>
+          </select>
+            </div>
+
             <div className="row d-flex justify-content-center flex-wrap m-0 gap-3 pb-5">
               {products.map((product, id) => (
                 <div key={id} className="mainCard p-1 rounded-1">
-                  {console.log(product.slug)}
-
                   <div className="productsCard">
                     <img
                       src={`https://mcbzesritumxqjtbullp.supabase.co/storage/v1/object/public/products/${product.image}?t=2023-09-19T13%3A20%3A01.474Z`}
                       className="card-img imgCard"
                       alt={product.name}
                     />
-
                     <div
                       className="card-body px-3 py-1 mt-2 card-info d-flex flex-column justify-conetnt-between"
                       style={{ backgroundColor: "white" }}
@@ -115,20 +75,19 @@ function Products() {
                         className="text-decoration-none productName"
                         to={`/products/${product.slug}`}
                       >
-                        <p className="card-title text-dark text-uppercase fw-bold product-name">
+                        <p className="card-title text-dark text-uppercase fw-bold mb-2 product-name">
                           {product.name}
                         </p>
                       </NavLink>
-
-                      <div className="d-flex justify-content-between align-items-center mt-1">
-                        <p className="card-text priceText fw-medium fst-italic mt-2 mb-2">
+                      <div className="d-flex justify-content-between align-items-baseline mt-1">
+                        <p className="card-text fw-normal ps-2 priceText">
                           $ {product.price}
                         </p>
                         <button
-                          className="addToCartButton1 px-2 py-1 d-flex align-items-center"
+                          className="addToCartButton1 fs-5 px-3 py-1 d-flex align-items-center"
                           onClick={() => handleAddCart(product)}
                         >
-                          <BsCartFill className="me-1" /> Add
+                          <BsCartFill className=" me-1" /> Add
                         </button>
                       </div>
                     </div>
