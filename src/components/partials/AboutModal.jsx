2af602b68c7a54f1e85aa3e748339ab73417c426
Leandro Matosas <lnmatosas@hotmail.com>
2023-09-25 17:05:50 -0300

    about

--- src/components/partials/AboutModal.jsx
+++ src/components/partials/AboutModal.jsx
@@ -12,11 +12,7 @@ function AboutModal({ modalAbout, setModalAbout, person }) {
             className="close-btn about-card-btn"
             onClick={() => setModalAbout(false)}
           />
-          <img
-            src="https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes-thumbnail.png"
-            alt=""
-            className="about-card-img"
-          />
+          <img src={person.image} alt="" className="about-card-img" />
           <div className="about-card-details">
             <h3 className="about-card-title">{person.name}</h3>
             <p>Full Stack Developer</p>
