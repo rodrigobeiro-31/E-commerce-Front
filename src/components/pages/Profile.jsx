1a507269991f807b57d59e607f593cbca505c49e
Kathia01 <kathia.olaverry@gmail.com>
2023-09-26 12:59:47 -0300

    profile responsive

--- src/components/pages/Profile.jsx
+++ src/components/pages/Profile.jsx
@@ -62,140 +62,147 @@ function Profile() {
 
   return (
     email && (
-      <div className="container-fluid d-flex flex-column justify-content-center p-0">
-        <div className="container-fluid d-flex align-items-center flex-column justify-content-center m-0 p-0 profile-banner">
-          <h2 className="text-white d-flex justify-content-center align-items-center title">
+     
+      <div className="container-fluid d-flex flex-column p-0">
+    <div className="container-fluid d-flex align-items-center justify-content-center m-0 p-0 profile-banner">
+        <h2 className="text-white text-center title">
             PROFILE
-          </h2>
-        </div>
-        <div className="container">
-          <div className="mb-5 m-5 p-5 text-white">
-            <h2 className="text-uppercase fw-bold contact-title d-flex align-items-center"> <BiSolidUserCircle className="me-2 profile-icon"/> My account</h2>
+        </h2>
+    </div>
+    <div className="container">
+        <div className="mb-5 mt-5 p-3 text-white">
+            <h2 className="text-uppercase fw-bold contact-title"> <BiSolidUserCircle className="me-2 profile-icon"/> My account</h2>
             <span className="line-span"></span>
             <p className="fw-normal contact-text">
-              Welcome <span className="fw-bold span-name">{firstname} {lastname}!</span>{" "}
-              From your Doppios's Account Dashboard, you have the ability to
-              edit your profile and track the status of your orders.
+                Welcome <span className="fw-bold span-name">{firstname} {lastname}!</span>{" "}
+                From your Doppios's Account Dashboard, you have the ability to
+                edit your profile and track the status of your orders.
             </p>
             <hr className="bg-white" />
-            <form onSubmit={handleSubmit} className="row g-3 mb-5">
-              <div className="col-6">
-                <div className="input-group mb-3">
-                  <span className="input-group-text profile-form-btn">
-                    <LiaUserEditSolid  className="text-white"/>
-                  </span>
-                  <input
-                    type="text"
-                    className="form-control contact-input"
-                    name="firstname"
-                    id="firstname"
-                    value={firstname}
-                    onChange={(e) => setFirstname(e.target.value)}
-                  />
+            <form onSubmit={handleSubmit} className="row g-3">
+                <div className="col-12 col-md-6">
+                    <div className="input-group">
+                        <span className="input-group-text profile-form-btn">
+                            <LiaUserEditSolid className="text-white img-fluid"/>
+                        </span>
+                        <input
+                            type="text"
+                            className="form-control contact-input"
+                            name="firstname"
+                            id="firstname"
+                            value={firstname}
+                            onChange={(e) => setFirstname(e.target.value)}
+                            
+                        />
+                    </div>
                 </div>
-              </div>
 
-              <div className="col-6">
-                <div className="input-group mb-3">
-                  <span className="input-group-text profile-form-btn">
-                    <LiaUserEditSolid className="text-white"/>
-                  </span>
-                  <input
-                    type="text"
-                    className="form-control contact-input"
-                    name="lastname"
-                    id="lastname"
-                    value={lastname}
-                    onChange={(e) => setLastname(e.target.value)}
-                  />
+                <div className="col-12 col-md-6">
+                    <div className="input-group">
+                        <span className="input-group-text profile-form-btn">
+                            <LiaUserEditSolid className="text-white img-fluid"/>
+                        </span>
+                        <input
+                            type="text"
+                            className="form-control contact-input"
+                            name="lastname"
+                            id="lastname"
+                            value={lastname}
+                            onChange={(e) => setLastname(e.target.value)}
+                          
+                        />
+                    </div>
                 </div>
-              </div>
-              <div className="col-12">
-                <div className="input-group mb-3">
-                  <span className="input-group-text profile-form-btn">
-                    <LiaUserEditSolid className="text-white" />
-                  </span>
-                  <input
-                    type="email"
-                    className="form-control contact-input"
-                    name="email"
-                    id="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
+                <div className="col-12">
+                    <div className="input-group">
+                        <span className="input-group-text profile-form-btn">
+                            <LiaUserEditSolid className="text-white img-fluid" />
+                        </span>
+                        <input
+                            type="email"
+                            className="form-control contact-input"
+                            name="email"
+                            id="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                           
+                        />
+                    </div>
                 </div>
-              </div>
 
-              <div className="col-6">
-                <div className="input-group mb-3">
-                  <span className="input-group-text profile-form-btn">
-                    {" "}
-                    <LiaUserEditSolid className="text-white"/>
-                  </span>
-                  <input
-                    type="password"
-                    className="form-control contact-input"
-                    name="password"
-                    id="password"
-                    placeholder="Password..."
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
+                <div className="col-12 col-md-6">
+                    <div className="input-group">
+                        <span className="input-group-text profile-form-btn">
+                            {" "}
+                            <LiaUserEditSolid className="text-white img-fluid"/>
+                        </span>
+                        <input
+                            type="password"
+                            className="form-control contact-input"
+                            name="password"
+                            id="password"
+                           
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
                 </div>
-              </div>
-              <div className="col-6 d-flex align-items-center justify-content-end m-0 ">
-                <button
-                  type="submit"
-                  className="d-flex align-items-center px-5 py-2 main-btn"
-                >
-                  SAVE CHANGES
-                </button>
-              </div>
+                <div className="col-12 col-md-6 d-flex align-items-center justify-content-md-end mb-3">
+                    <button
+                        type="submit"
+                        className="btn btn-primary px-4 py-2 main-btn"
+                    >
+                        SAVE CHANGES
+                    </button>
+                </div>
             </form>
             <h2 className="text-uppercase fw-bold contact-title">
-              Order tracking
+                Order tracking
             </h2>
             <span className="line-span"></span>
-            <table className="table">
-              <thead>
-                <tr>
-                  <th scope="col">Order ID</th>
-                  <th scope="col">Date</th>
-                  <th scope="col">Products</th>
-                  <th scope="col">Status</th>
-                </tr>
-              </thead>
-              <tbody>
-                {orders.length > 0 &&
-                  orders.map((order, id) => (
-                    <tr key={id}>
-                      <th scope="row">{order._id}</th>
-                      <td>
-                        {format(new Date(order.createdAt), "MMMM dd, yyyy")}
-                      </td>
-                      <td>
-                        <NavLink
-                          className=""
-                          onClick={() => {
-                            handleShow();
-                            setSelectedCart(order.cart);
-                          }}
-                        >
-                          View products
-                        </NavLink>
-                      </td>
-                      <td>{order.status}</td>
-                      <ModalOrder
-                        handleClose={handleClose}
-                        show={show}
-                        cart={selectedCart}
-                      />
-                    </tr>
-                  ))}
-              </tbody>
-            </table>
-          </div>
+            <div className="table-responsive">
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Order ID</th>
+                            <th scope="col">Date</th>
+                            <th scope="col">Products</th>
+                            <th scope="col">Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {orders.length > 0 &&
+                            orders.map((order, id) => (
+                                <tr key={id}>
+                                    <th scope="row">{order._id}</th>
+                                    <td>
+                                        {format(new Date(order.createdAt), "MMMM dd, yyyy")}
+                                    </td>
+                                    <td>
+                                        <NavLink
+                                            className=""
+                                            onClick={() => {
+                                                handleShow();
+                                                setSelectedCart(order.cart);
+                                            }}
+                                        >
+                                            View products
+                                        </NavLink>
+                                    </td>
+                                    <td>{order.status}</td>
+                                    <ModalOrder
+                                        handleClose={handleClose}
+                                        show={show}
+                                        cart={selectedCart}
+                                    />
+                                </tr>
+                            ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
-      </div>
+    </div>
+</div>
+
     )
   );
 }
