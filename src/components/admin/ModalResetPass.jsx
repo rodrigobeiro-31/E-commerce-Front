ab8889dcecc70e5c49f61b6aac065775f8f882cb
rodrigobeiro-31 <rodrigo.beiro@student.ha.dev>
2023-09-27 09:28:31 -0300

    Fixing reset password

--- src/components/admin/ModalResetPass.jsx
+++ src/components/admin/ModalResetPass.jsx
@@ -5,13 +5,12 @@ import axios from "axios";
 
 export default function ModalResetPass() {
   const [showModal, setShowModal] = useState(false);
-  const [email, setEmail] = useState(false);
+  const [email, setEmail] = useState("");
 
   const handleClose = () => setShowModal(false);
   const handleShow = () => setShowModal(true);
 
   const handleSubmit = () => {
-    console.log(email);
     ResetPass();
     handleClose();
   };
@@ -41,8 +40,8 @@ export default function ModalResetPass() {
           <Form onSubmit={handleSubmit} />
           <Form.Group controlId="resetEmail">
             <Form.Label>
-              Write your email in the following field and we will send you a
-              notification to your email.{" "}
+              Write your email in the field below, and we will send you a
+              notification to your email.
             </Form.Label>
             <Form.Control
               type="text"
@@ -57,9 +56,8 @@ export default function ModalResetPass() {
         </Modal.Body>
 
         <Modal.Footer>
-          <Button onClick={handleClose}>Close</Button>
           <Button variant="primary" onClick={handleSubmit}>
-            Send reset{" "}
+            Submit{" "}
           </Button>
         </Modal.Footer>
       </Modal>
